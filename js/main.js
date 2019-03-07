3ba985bd95738b8c4aa3f673e1b69e0885bf09f3
SURESH KANNAN <sureshk4.info@gmail.com>
2019-03-07 13:40:09 +0530

    Service Worker Example V1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Make sure sw supported
+if( 'serviceWorker' in navigator)
+{
+    //console.log('Service Worker Supported');
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../sw_cached_pages.js')
+            .then(reg => console.log('Service Worker: Registered'))
+            .catch(err => console.log('Service Woker: Error: ${err}'))
+    })
+}
